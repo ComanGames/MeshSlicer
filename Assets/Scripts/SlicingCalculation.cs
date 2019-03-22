f5af18f9d8b2974b67d3e5ddc941915c35f113a1
Yuriy Paramonov <coman.games@outlook.com>
2019-03-23 00:48:45 +0200

    Add first logical class

---
+++ Assets/Scripts/SlicingCalculation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts{
+	public class SlicingCalculation{
+		public Mesh[] Slice(Mesh mesh, Vector3 pointA, Vector3 pointB, Vector3 direction){
+
+			 throw new NotImplementedException();
+		}
+		
+	}
+}
