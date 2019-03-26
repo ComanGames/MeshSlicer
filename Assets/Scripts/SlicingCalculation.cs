b93af4234bdaabe81b70195c408a4312ac1467e8
Yuriy Paramonov <coman.games@outlook.com>
2019-03-26 15:21:43 +0200

    I Changed algo a liitle bit and it's working

--- Assets/Scripts/SlicingCalculation.cs
+++ Assets/Scripts/SlicingCalculation.cs
@@ -67,109 +67,44 @@ namespace Assets.Scripts{
 			}
 			Debug.Log(tIntersection.Count);
 
-			List<Vector3> edge = new List<Vector3>();
+			List<Vector3> dots = new List<Vector3>();
 			
-			LinkedList<Triangle> tLinked = GetLinkedList(tIntersection);
-			Debug.Log("Linked list count"+ tLinked.Count);
 
 
 			//Get First Vetices
-			Vector3 dot ;
 //				= GetDotFromEdge(tLinked, tLinked.First.Value, tLinked.Last.Value);
 //			edge.Add(dot);
-
-			LinkedListNode<Triangle> current = tLinked.First;
-			for (int i = 0; i < tLinked.Count-1; i++){
-				dot = GetDotFromEdge(tLinked, current.Value, current.Next.Value);
-				current = current.Next;
-				edge.Add(dot);
+			Vector3[] v = mesh.vertices;
+			foreach (Triangle t in tIntersection){
+				Vector3 d = (v[t.A] + v[t.B] + v[t.C])/3f;
+				dots.Add(d);
 			}
 
-			return edge.ToArray();
+			
+
+			return dots.ToArray();
 		}
 
-		private Vector3 GetDotFromEdge(LinkedList<Triangle> tLinked, Triangle triangleA, Triangle triangleB){
-			int normalA;
-			int normalB;
-			GetSameEdge(triangleA,triangleB, out normalA, out normalB);
-			Vector3 vertA = mesh.vertices[normalA];
-			Vector3 vertB = mesh.vertices[normalB];
-
-			Vector3 dot = UnityEngine.Vector3.Lerp(vertA, vertB, 0.5f);
-			return dot;
-		}
-
-		public  LinkedList<Triangle> GetLinkedList(List<Triangle> input){
-
-
-			LinkedList<Triangle> outlineLinked = new LinkedList<Triangle>();
-			outlineLinked.AddFirst(input[0]);
-			//We just add one more Item
-			Debug.Log(input.Count);
-			LinkedListNode<Triangle> current = null;
-
-			for (int i = 1; i < input.Count; i++){
-				if (HaveSameEdge(input[i],input[0]) && !input[i].Equals(input[0])){
-					current = outlineLinked.AddAfter(outlineLinked.First, input[i]);
-					break;
-				}
-			}
-
-			for (int j = 0; j < input.Count; j++){
-				
-				
-			for (int i = 0; i < input.Count; i++){
-				if (!input[i].Equals(current.Value)
-				    && !input[i].Equals(current.Previous.Value) 
-				    && HaveSameEdge(input[i],current.Value))
-				{
-					current= outlineLinked.AddAfter(current, input[i]);
-				}
-
-			}
-			}
-
-
-			return outlineLinked;
-		}
 
 		private bool HaveSameEdge(Triangle triangleA, Triangle triangleB){
-			int normalA = 0;
-			int normalB = 0;
-			GetSameEdge(triangleA, triangleB, out normalA, out normalB);
-			return normalA != -1 && normalB != -1;
+			return GetSameEdge(triangleA,triangleB) != null;
 		}
 
 
-		private void GetSameEdge(Triangle triangleA, Triangle triangleB, out int normalA, out int normalB){
-			normalA = -1;
-			normalB = -1;
+		private Edge GetSameEdge(Triangle triangleA, Triangle triangleB){
+			Edge[] edgesA = triangleA.Edges;
+			Edge[] edgesB = triangleB.Edges;
 
-			Vector3 aa = mesh.vertices[triangleA.A];
-			Vector3 ab = mesh.vertices[triangleA.B];
-			Vector3 ac = mesh.vertices[triangleA.C];
-
-			Vector3 ba = mesh.vertices[triangleB.A];
-			Vector3 bb = mesh.vertices[triangleB.B];
-			Vector3 bc = mesh.vertices[triangleB.C];
-
-			Vector3[] a = new[]{aa, ab, ac};
-			Vector3[] b = new[]{ba, bb, bc};
-
-			int count = 0;
-			for (int i = 0; i < a.Length; i++){
-				for (int j = 0; j < b.Length; j++){
-
-					if (a[i] == b[j]){
-						if(count==0)
-							triangleA=a[i]
+			for (int i = 0; i < edgesA.Length; i++){
+				for (int j = 0; j < edgesB.Length; j++){
+					if (edgesA[i].Equals(edgesB[j])){
+						edgesB[j] = edgesA[i];
+						return edgesA[i];
 					}
-
-					
 				}
 				
 			}
-
+			return null;
 		}
 
 		private bool IsTriangleAbove(Triangle triangle, Vector3 pos, Quaternion rot){
--- Assets/Scripts/Triangle.cs
+++ Assets/Scripts/Triangle.cs
@@ -1,25 +1,27 @@
 ﻿using System;
-using System.Linq;
-using UnityEngine;
 
 namespace Assets.Scripts{
-	public struct Triangle:IEquatable<Triangle>{
+	public class Triangle:IEquatable<Triangle>{
 		public int A;
 		public int B;
 		public int C;
 
+		public Edge[] Edges; //AC
 
 		public Triangle(int a, int b, int c){
 			this.A = a;
 			this.B = b;
 			this.C = c;
+			Edges = new Edge[3];
+
+			Edges[0] = new Edge(A,B);
+			Edges[1] = new Edge(B,C);
+			Edges[2] = new Edge(A,C);
 		}
 
 		public int[] GerVertices(){
 			return new[]{A, B, C};
 		}
-
-
 		public bool Equals(Triangle other){
 			return A == other.A && B == other.B && C == other.C;
 		}
